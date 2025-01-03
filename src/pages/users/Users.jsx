c05c5a3fa97adaa40c6e42890a95c5b0443c3e02
samucop95 <samucop@icloud.com>
2025-01-03 13:57:05 +0100

    pagina de usuarios creada

---
+++ src/pages/users/Users.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import Navbar from "../../components/navbar/NavBar";
+import UserList from "../../components/user-list/UserList";
+import UserProfile from "../../components/user-profile/UserProfile";
+import { getAllUsers } from "../../api/user";
+import "./Users.css";
+
+function Users() {
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const data = await getAllUsers();
+                setUsers(data);
+            } catch (error) {
+                console.error("Error al cargar usuarios", error);
+            }
+        }
+        fetchUsers();
+    }, []);
+
+    function handleUserCreated(newUser) {
+        setUsers([...users, newUser]); // Actualiza la lista localmente
+    }
+    function handleUserUpdated(updatedUser) {
+        setUsers(users.map(user => user.user_id === updatedUser.user_id ? updatedUser : user));
+        if (selectedUser?.user_id === updatedUser.user_id) {
+            setSelectedUser(updatedUser);
+        }
+    }
+
+    function handleUserDeleted(userId) {
+        setUsers(users.filter(user => user.user_id !== userId));
+        setSelectedUser(null); // Limpia el perfil seleccionado si fue eliminado
+    }
+
+    return (
+        <div className="users-page">
+            <Navbar />
+            <section className="user-list-container">
+                <UserList
+                    users={users}
+                    onSelectUser={setSelectedUser}
+                    onCreateUser={handleUserCreated}
+                />
+            </section>
+            <section className="user-profile-container">
+                <UserProfile
+                    user={selectedUser}
+                    onUserUpdated={handleUserUpdated}
+                    onUserDeleted={handleUserDeleted}
+                />
+            </section>
+        </div>
+    )
+}
+
+export default Users;
