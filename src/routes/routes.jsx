b27cf730f526cd181ee6381e361c50eb99666970
samucop95 <samucop@icloud.com>
2025-01-03 13:56:23 +0100

    ruta usuarios habilitada

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,9 +4,9 @@ import Root from "../pages/Root";
 import Login from "../pages/login/Login";
 import LandingPage from "../pages/landingpage/LandingPage";
 import Menu from "../pages/menu/Menu";
+import Users from "../pages/users/Users";
 //import Home from "../pages/Home";
 //import Tables from "../pages/Tables";
-//import Users from "../pages/Users";
 //import History from "../pages/History";
 
 const router = createBrowserRouter([
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         path: "/",              // La ruta base de la URL
         element: <Root />,      // El componente que se renderizará
         errorElement: <ErrorPage />,  // Qué mostrar si hay un error
-        children: [ 
+        children: [
             {
                 path: "/",
                 element: <LandingPage />,
@@ -27,22 +27,22 @@ const router = createBrowserRouter([
                 path: "menu",
                 element: <Menu />,
             },
-//            {
-//                path: "home",          // Página de inicio (misma que '/')
-//                element: <Home />,
-//            },
-//            {
-//                path: "tables",       // Resultará en '/tables'
-//                element: <Tables />,
-//            },
-//            {
-//                path: "users",        // Resultará en '/users'
-//                element: <Users />,
-//            },
-//            {
-//                path: "history",      // Resultará en '/history'
-//                element: <History />,
-//            },
+            {
+                path: "users",        // Resultará en '/users'
+                element: <Users />,
+            },
+            //            {
+            //                path: "home",          // Página de inicio (misma que '/')
+            //                element: <Home />,
+            //            },
+            //            {
+            //                path: "tables",       // Resultará en '/tables'
+            //                element: <Tables />,
+            //            },
+            //            {
+            //                path: "history",      // Resultará en '/history'
+            //                element: <History />,
+            //            },
         ],
     },
 ]);
