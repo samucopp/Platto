2de5a661a0135cb8186b5fabc35b1680428e24c8
samucop95 <samucop@icloud.com>
2025-01-05 14:32:41 +0100

    reestructuracion de las rutas

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import LayoutWithNavbar from "../src/layouts/LayoutWithNavbar";
+
+import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/login/Login";
+import LandingPage from "./pages/landingpage/LandingPage";
+import Menu from "./pages/menu/Menu";
+import Users from "./pages/users/Users";
+import History from "./pages/history/History";
+//import Home from "../pages/Home";
+//import Tables from "../pages/Tables";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="login" element={<Login />} />
+
+            <Route element={<LayoutWithNavbar />}>
+                <Route path="menu" element={<Menu />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/history" element={<History />} />
+                {/* Agrega aquí todas las páginas que quieras que tengan Navbar */}
+            </Route>
+
+            <Route path="*" element={<ErrorPage />} />
+        </Routes>
+    );
+}
+
+export default App;
