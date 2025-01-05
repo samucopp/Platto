963f7e945d98f907bfb645ea44f3b5c42de77608
samucop95 <samucop@icloud.com>
2025-01-05 13:08:08 +0100

    componente para visualizar un resumen de las comandas existentes en el historial creado y en funcionamiento

---
+++ src/components/command-list/CommandList.jsx
@@ -0,0 +1,26 @@
+import './CommandList.css';
+
+function CommandList({ commands, onSelectCommand }) {
+    return (
+        <section className="command-list-container">
+            <h2>Comandas</h2>
+            <ul>
+                {commands.length > 0 ? (
+                    commands.map((command) => (
+                        <li key={command._id} onClick={() => onSelectCommand(command.command_id)}>
+                            <p>Comanda #{command.command_id}</p>
+                            <p>Fecha: {new Date(command.date).toLocaleDateString()}</p>
+                            <p>Total: {(command.total / 100).toFixed(2)} €</p>
+                            <p>Estado: {command.status}</p>
+                            <p>Mesa: {command.table_id} - Comensales: {command.pax}</p>
+                        </li>
+                    ))
+                ) : (
+                    <p>No hay comandas cerradas disponibles.</p>
+                )}
+            </ul>
+        </section>
+    );
+}
+
+export default CommandList;
